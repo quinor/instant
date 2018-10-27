9f8d6d6bc26d3ad7b65b8c29bd1b0f4676228ec4
Wojciech Jabłoński <wj359634@students.mimuw.edu.pl>
2018-10-27 14:53:34 +0200

    bugfix in java backend optimization

--- instc/backends.py
+++ instc/backends.py
@@ -89,7 +89,7 @@ def jvm_backend(program, classname):
         if isinstance(tree, ast.Application):
             left, lh = optimize(tree.left)
             right, rh = optimize(tree.right)
-            if lh < rh:
+            if lh < rh and tree.function.symbol in "*+":
                 right, left = left, right
                 rh, lh = lh, rh
             return ast.Application(left, right, tree.function), max(lh, rh+1)
